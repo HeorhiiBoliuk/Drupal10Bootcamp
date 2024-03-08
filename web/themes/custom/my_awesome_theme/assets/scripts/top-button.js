7180f4be9f941d9b96d3872413689a7abaa4ff1a
Heorhii Boliuk <h.boliuk@dev-branch.com>
2024-03-08 17:27:29 +0200

    #7644 Add a dynamic div creation in script with image

--- web/themes/custom/my_awesome_theme/assets/scripts/top-button.js
+++ web/themes/custom/my_awesome_theme/assets/scripts/top-button.js
@@ -2,26 +2,34 @@
  * Back to Top scrolling.
  */
 
-(function (Drupal, once){
-  Drupal.behaviors.backToTop = { attach:function (context) {
-    once("backToTop", '.scroll-top', context).forEach( function (element){
-      element.addEventListener('click', function (e){
-        window.scrollTo({
-          top: 0,
-          behavior: 'smooth',
+(function (Drupal, once) {
+  Drupal.behaviors.backToTop = {
+    attach: function (context) {
+      once("backToTop", '.footer_bottom', context).forEach(function (element) {
+        let backToTopButton = document.createElement('div');
+        backToTopButton.classList.add('scroll');
+        let img = document.createElement('img');
+        img.src = '/themes/custom/my_awesome_theme/assets/images/up-arrow-png-8.png';
+        backToTopButton.appendChild(img);
+        element.appendChild(backToTopButton);
+
+        backToTopButton.addEventListener('click', function (e) {
+          window.scrollTo({
+            top: 0,
+            behavior: 'smooth',
+          });
+        });
+
+        window.addEventListener('scroll', function (e) {
+          let high = window.innerHeight;
+          let scrollProgress = window.scrollY;
+          if (scrollProgress > high * 0.1) {
+            backToTopButton.classList.add('open');
+          } else {
+            backToTopButton.classList.remove('open');
+          }
         });
       });
-
-      window.addEventListener('scroll', function (e) {
-        let high = window.innerHeight;
-        let scrollProgress = window.scrollY;
-        if (scrollProgress > high * 0.1){
-          element.classList.add('open');
-        }
-        else{
-          element.classList.remove('open');
-        }
-      });
-    });
-    }};
+    }
+  };
 }(Drupal, once));
