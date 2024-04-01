aee6913d750ecf9da617f41d09530751d6eef7c6
Heorhii Boliuk <h.boliuk@dev-branch.com>
2024-04-01 17:20:59 +0300

    Task#7752 Added a search by part of word, limited the search to 5 characters, Created a new content type and indexed it in Solr, added a new field type location for indexing this content type field

---
+++ web/modules/custom/custom_location/src/Plugin/search_api/data_type/CustomLocation.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Drupal\custom_location\Plugin\search_api\data_type;
+
+use Drupal\search_api\DataType\DataTypePluginBase;
+
+/**
+ * Provides a custom full text data type.
+ *
+ * @SearchApiDataType(
+ *   id = "custom_location",
+ *   label = @Translation("Location Custom"),
+ *   description = @Translation("Custom location field type."),
+ *   fallback_type = "text",
+ *   prefix = "rpt"
+ * )
+ */
+class CustomLocation extends DataTypePluginBase {}
