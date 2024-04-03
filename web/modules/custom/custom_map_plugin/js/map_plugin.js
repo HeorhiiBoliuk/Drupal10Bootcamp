943422bcb5e90297fada65195385b922fa7e5a2a
Heorhii Boliuk <h.boliuk@dev-branch.com>
2024-04-03 14:21:58 +0300

    #7826

---
+++ web/modules/custom/custom_map_plugin/js/map_plugin.js
@@ -0,0 +1,53 @@
+(function (Drupal, once, drupalSettings) {
+  'use strict';
+
+  Drupal.behaviors.customMapBehavior = {
+    attach: function (context, settings) {
+      once('customMapBehavior', '#leaflet-map', context).forEach(function (){
+        const myCustomColour = settings.customMapView.color;
+        const myCustomSize = settings.customMapView.size;
+        const myCustomZoom = settings.customMapView.zoom;
+
+        const locations = settings.locations_stores.locations;
+
+        var markers = L.layerGroup();
+
+        const markerHtmlStyles = `
+        background-color: ${myCustomColour};
+        width: ${myCustomSize}rem;
+        height: ${myCustomSize}rem;
+        display: block;
+        left: -1.5rem;
+        top: -1.5rem;
+        position: relative;
+        border-radius: 3rem 3rem 0;
+        transform: rotate(45deg);
+        border: 1px solid #FFFFFF;`;
+
+        locations.forEach(function(location) {
+          const latitude = location.latitude;
+          const longitude = location.longitude;
+
+          var point = [latitude, longitude];
+
+          var marker = L.marker(point, {
+            icon: L.divIcon({
+              className: "my-custom-pin",
+              html: `<span style="${markerHtmlStyles}" />`
+            })
+          });
+          marker.addTo(markers);
+        });
+
+        var map = L.map('leaflet-map').setView([51.505, -0.09], myCustomZoom);
+
+        markers.addTo(map);
+
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+          attribution: '© OpenStreetMap contributors'
+        }).addTo(map);
+      });
+    }
+  };
+
+})(Drupal, once, drupalSettings);
---
+++ web/modules/custom/custom_map_plugin/src/Plugin/views/area/StoreLocations.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace Drupal\custom_map_plugin\Plugin\views\area;
+
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\views\Plugin\views\area\AreaPluginBase;
+
+/**
+ * Provides settings for a custom map view area.
+ *
+ * @ingroup views_area_handlers
+ *
+ * @ViewsArea("custom_map_settings")
+ */
+class StoreLocations extends AreaPluginBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function defineOptions() {
+    $options = parent::defineOptions();
+
+    $options['color'] = ['default' => '#FF0000'];
+    $options['size'] = ['default' => '2'];
+    $options['zoom'] = ['default' => '13'];
+
+    return $options;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildOptionsForm(&$form, FormStateInterface $form_state) {
+    parent::buildOptionsForm($form, $form_state);
+
+    $form['color'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Map Color'),
+      '#default_value' => $this->options['color'],
+    ];
+
+    $form['size'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Marker Size'),
+      '#description' => $this->t('Enter the marker size (in pixels)'),
+      '#default_value' => $this->options['size'],
+    ];
+
+    $form['zoom'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Map Zoom'),
+      '#description' => $this->t('Enter the map zoom level'),
+      '#default_value' => $this->options['zoom'],
+    ];
+  }
+
+  /**
+   * Return stores location.
+   */
+  public function getStoresLocation(): array {
+    $locations = [];
+    $location_entities = $this->view->result;
+    foreach ($location_entities as $location_entity) {
+      $location_values = $location_entity->_entity->get('field_location')->getValue();
+      if (!empty($location_values)) {
+        $latitude = $location_values[0]['lat'];
+        $longitude = $location_values[0]['lon'];
+        $locations[] = [
+          'latitude' => $latitude,
+          'longitude' => $longitude,
+        ];
+      }
+    }
+    return $locations;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function render($empty = FALSE) {
+    $locations = $this->getStoresLocation();
+    if (!$empty || !empty($this->options['empty'])) {
+      return [
+        '#markup' => '<div id="leaflet-map"></div>',
+        '#attached' => [
+          'drupalSettings' => [
+            'locations_stores' => [
+              'locations' => $locations,
+            ],
+            'customMapView' => [
+              'color' => $this->options['color'],
+              'size' => $this->options['size'],
+              'zoom' => $this->options['zoom'],
+            ],
+          ],
+        ],
+      ];
+    }
+    return NULL;
+  }
+
+}
